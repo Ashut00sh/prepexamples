14329c9eeb1569c83aff0df3864a3a7c99ab38f7
Ashut00sh <jadhavashutosh42@gmail.com>
2020-10-14 07:52:41 +0530

    try

--- Repeatele.java
+++ Repeatele.java
@@ -2,20 +2,31 @@ import java.util.Scanner;
 
 public class Repeatele {
     public static void main(String[] args) {
+        int a;
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        System.out.println("Enter min size of array=3");
+        a = sc.nextInt();
         int arr[] = new int[a];
-        int brr[] = new int[a];
-        for (int i = 0; i < 3; i++) {
+        int brr[] = new int[a * arr.length];
+        // int brr[] = new int[a];
+        System.out.println("Enter  3 array element ");
+        for (int i = 0; i < a; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int j = 0; j < arr.length - 1; j++) {
+        // System.out.println(arr.length);
+        for (int j = 0; j < arr.length; j++) {
             // int x = arr[j];
-            for (int k = 0; k < 3; k++) {
-                brr[k] = arr[j];
+            for (int k = 0; k < arr.length; k++) {
+
+                brr[j] = arr[k];
+
             }
+
         }
-        System.out.println(brr);
+        System.out.println("array elements is ==");
+        for (int ab : brr) {
+            System.out.println(ab);
+        }
         sc.close();
     }
 }
