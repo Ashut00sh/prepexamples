1dc92a0d398677e9d7b3728e8ad7fde7ac804b2d
Ashut00sh <jadhavashutosh42@gmail.com>
2020-10-14 08:32:54 +0530

    compare i to array value with flag

--- Missnum.java
+++ Missnum.java
@@ -4,26 +4,30 @@ class Missnum {
     public static void main(String[] args) {
         int j;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
         int a = sc.nextInt();
         int arr[] = new int[a];
         // int arr[]={1,3,5,6,7,9};
+        System.out.println("Enter array elements");
         for (j = 1; j <= 5; j++) {
             arr[j] = sc.nextInt();
         }
         System.out.println("array element s is====");
-        // for (int i = 1; i < arr.length; i++) {
-         int ar1[]=new int [10];
-         int ct=1;
-         
-        for (int i = 0; i <= arr.length - 1; i++) {
 
-            if (arr[i] > ct) {
-                ar1[ct]=ct;
-                ct++;
-                System.out.println(i);
-            }else if(arr[i])
+        for (int i = 1; i <= 10; i++) {
+            boolean flag = false;
+            for (int k = 0; k < arr.length; k++) {
+
+                if (i == arr[k]) {
+                    flag = true;
+                }
+            }
+            if (flag == false) {
+                System.out.print(" ," + i);
+            }
         }
+        System.out.println();
         sc.close();
-
     }
+
 }
