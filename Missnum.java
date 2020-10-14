af313d862cf9e722c9601be5a22384f8155ae37a
Ashut00sh <jadhavashutosh42@gmail.com>
2020-10-14 08:42:38 +0530

    ui

--- Missnum.java
+++ Missnum.java
@@ -8,13 +8,15 @@ class Missnum {
         int a = sc.nextInt();
         int arr[] = new int[a];
         // int arr[]={1,3,5,6,7,9};
-        System.out.println("Enter array elements");
-        for (j = 1; j <= 5; j++) {
+        System.out.println("Enter how many array elements want to enter out of" + arr.length);
+        int b = sc.nextInt();
+        System.out.println("Enter" + " " + b + " " + "elements into array");
+        for (j = 0; j < b; j++) {
             arr[j] = sc.nextInt();
         }
-        System.out.println("array element s is====");
+        System.out.println("Misssing array elements is====");
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= a; i++) {
             boolean flag = false;
             for (int k = 0; k < arr.length; k++) {
 
